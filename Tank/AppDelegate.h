74ac200fb81119c2a3f2c6d3aa879fae9c77022f
Changhao Yang <changhay@andrew.cmu.edu>
2020-11-20 15:20:31 -0500

    Initial Commit

---
+++ Tank/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Tank
+//
+//  Created by Changhao Yang on 11/20/20.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
